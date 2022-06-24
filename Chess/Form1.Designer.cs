aac72ede6c6e6011f7d9c9f985f0ed1405bca98d
Kebaru <kebaru@yandex.ru>
2022-06-24 13:29:42 +0900

    1

--- Chess/Form1.Designer.cs
+++ Chess/Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.ClientSize = new System.Drawing.Size(464, 401);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Chess_Kudyakov";
             this.ResumeLayout(false);
 
         }
